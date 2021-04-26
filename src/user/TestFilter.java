c25e760719e71b8dbe48e9ab6203f546f94608a4
Dillon DeGarmo <cddegarmo@gmail.com>
2021-04-26 11:33:59 -0500

    Add client code to test Predicate

--- src/user/TestFilter.java
+++ src/user/TestFilter.java
@@ -1,5 +1,28 @@
 package user;
 
-public class TestFilter {
+import appclasses.*;
+import appclasses.Specimen.*;
+import appclasses.Test.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestFilter {
+   public static void main(String[] args) {
+      Patient patient = Patient.addRecord("Wilson");
+      LocalDate collected = LocalDate.of(2021, 4, 11);
+      Specimen specimen = Specimen.harvest(Source.BRAIN, Matter.TISSUE, collected, 48);
+      Test test1 = new Test(Microbe.BACTERIUM, Type.CULTURE);
+      Test test2 = new Test(Microbe.PARASITE, Type.STAIN);
+      Test test3 = new Test(Microbe.VIRUS, Type.DIRECT);
+      Test test4 = new Test(Microbe.VIRUS, Type.SCREEN);
+      List<Test> tests = new ArrayList<>();
+      tests.add(test1);
+      tests.add(test2);
+      tests.add(test3);
+      tests.add(test4);
+      Sample sample = new Sample(patient, specimen, tests);
+      System.out.println(sample.toString());
+   }
 }
